18bc1bc83ea1ced7c0078b323fb7fce258c2a8b4
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-07-20 14:39:05 +0800

    initial commit

---
+++ src/hooks.server.ts
@@ -0,0 +1,36 @@
+import { SvelteKitAuth } from '@auth/sveltekit';
+import GoogleProvider from '@auth/core/providers/google';
+import { env } from '$env/dynamic/private';
+import type { Provider } from '@auth/core/providers';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from '$lib/data/prisma';
+
+export const handle = SvelteKitAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope:
+            'https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'
+        }
+      }
+    }) as Provider
+  ],
+  pages: {
+    error: '/signup/error'
+  },
+  cookies: {
+    pkceCodeVerifier: {
+      name: 'sveltekit-auth.pkce.code_verifier',
+      options: {
+        httpOnly: true,
+        sameSite: 'none',
+        path: '/',
+        secure: true
+      }
+    }
+  }
+});
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { signIn } from '@auth/sveltekit/client';
+</script>
+
+<h1>Boringtube</h1>
+<button
+  class="rounded-md bg-red-500 px-4 py-2 text-white"
+  on:click={() => {
+    signIn('google');
+  }}>Sign In to Google</button>
