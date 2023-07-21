ef90e18bf971c85be06686631ccc973c966d8ec7
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-07-21 20:31:31 +0800

    unarchive

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -75,6 +75,17 @@
     $videosQuery.refetch();
   }
 
+  function unarchiveVideo(video: Video) {
+    playing = false;
+    archivedVideos = archivedVideos.filter((v) => {
+      return v.id !== video.id;
+    });
+    archivedVideoIds = archivedVideoIds.filter((v) => {
+      return v !== video.id;
+    });
+    $videosQuery.refetch();
+  }
+
   $: {
     if (view === 'feed' && $videosQuery.data) {
       viewingVideoList = $videosQuery.data;
@@ -160,20 +171,36 @@
           <button
             class="absolute bottom-2 right-2 hidden w-8 rounded-md p-1 text-zinc-700 hover:bg-zinc-900 group-hover:block"
             on:click|stopPropagation={() => {
-              archiveVideo(video);
-            }}
-            ><svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="h-full w-full">
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-            </svg>
+              if (view === 'feed') archiveVideo(video);
+              else unarchiveVideo(video);
+            }}>
+            {#if view === 'feed'}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="h-full w-full">
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+              </svg>
+            {:else}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="h-full w-full">
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+              </svg>
+            {/if}
           </button>
         </button>
       {/each}
@@ -218,8 +245,17 @@
         <button
           class="rounded-md bg-zinc-800 px-4 py-2 font-bold text-zinc-400 transition duration-200 hover:bg-zinc-900"
           on:click={() => {
-            if (currentVideo) archiveVideo(currentVideo);
-          }}>Archive</button>
+            if (currentVideo) {
+              if (view === 'feed') archiveVideo(currentVideo);
+              else unarchiveVideo(currentVideo);
+            }
+          }}>
+          {#if view === 'feed'}
+            Archive
+          {:else}
+            Unarchive
+          {/if}
+        </button>
       </div>
       <div class="mt-2 flex gap-2">
         <div class="w-14">
