8af195a2382c5a7e9eb2babccd568ead87218afc
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-07-24 21:47:48 +0800

    archive all

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -15,7 +15,7 @@
   let playing = false;
 
   let viewingVideoList: Video[] = [];
-  let view: 'new' | 'archived' = 'new';
+  let view: 'new' | 'archived' | 'watch_later' = 'new';
 
   let theaterMode = false;
 
@@ -126,6 +126,25 @@
     $videosQuery.refetch();
   }
 
+  function archiveAll() {
+    const localAllVideosString = localStorage.getItem('all_videos');
+    const localAllVideos: Video[] = localAllVideosString ? JSON.parse(localAllVideosString) : [];
+    const updatedAllVideos = localAllVideos.map((v) => {
+      if (v.status === 'NEW') {
+        v.status = 'ARCHIVED';
+      }
+      return v;
+    });
+    localStorage.setItem('all_videos', JSON.stringify(updatedAllVideos));
+    $videosQuery.refetch();
+
+    const dialog = document.getElementById('archiveAllConfirmation');
+    if (dialog) {
+      //@ts-ignore
+      dialog.close();
+    }
+  }
+
   $: {
     if ($videosQuery.data) {
       viewingVideoList = $videosQuery.data.pages.flatMap((page) => {
@@ -173,7 +192,7 @@
         </div>
 
         <div class="relative flex flex-1 flex-col overflow-scroll bg-zinc-950 px-3 pb-3">
-          <div class="sticky top-0 z-50 flex gap-2 bg-zinc-950 p-3">
+          <div class="sticky top-0 z-50 flex place-items-center gap-2 bg-zinc-950 py-3">
             <button
               class="rounded-md px-4 py-1 transition duration-100 {view === 'new'
                 ? 'bg-zinc-50 text-zinc-900 hover:bg-zinc-200'
@@ -183,6 +202,14 @@
                 $videosQuery.refetch();
               }}>New</button>
             <button
+              class="rounded-md px-4 py-1 transition duration-100 {view === 'watch_later'
+                ? 'bg-zinc-50 text-zinc-900 hover:bg-zinc-200'
+                : 'bg-zinc-700 text-white hover:bg-zinc-600'}"
+              on:click={() => {
+                view = 'watch_later';
+                $videosQuery.refetch();
+              }}>Watch Later</button>
+            <button
               class="rounded-md px-4 py-1 transition duration-100 {view === 'archived'
                 ? 'bg-zinc-50 text-zinc-900 hover:bg-zinc-200'
                 : 'bg-zinc-700 text-white hover:bg-zinc-600'}"
@@ -190,6 +217,35 @@
                 view = 'archived';
                 $videosQuery.refetch();
               }}>Archived</button>
+
+            {#if view === 'new' || view === 'archived'}
+              <button
+                class="group absolute right-0 block w-10 rounded-md bg-zinc-800 px-2 py-1 text-zinc-500 transition duration-200 hover:bg-zinc-700"
+                on:click={() => {
+                  const dialog = document.getElementById('archiveAllConfirmation');
+                  if (dialog) {
+                    //@ts-ignore
+                    dialog.showModal();
+                  }
+                }}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="h-full w-full">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                </svg>
+                <div
+                  class="absolute right-0 top-full mt-2 hidden w-24 rounded-md bg-red-900 px-2 py-2 text-red-300 shadow-md group-hover:block">
+                  Archive all
+                </div>
+              </button>
+            {/if}
           </div>
           {#if $videosQuery.isLoading}
             <div class="m-auto w-10 fill-zinc-500">
@@ -276,7 +332,7 @@
     </div>
   </div>
 
-  <div class="relative flex-1 overflow-scroll bg-zinc-950">
+  <div class="relative z-10 flex-1 overflow-scroll bg-zinc-950">
     {#if currentVideo}
       {#if playing}
         <iframe
@@ -284,7 +340,7 @@
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          class={theaterMode ? 'h-full w-full' : 'h-4/5 w-full'}
+          class={theaterMode ? 'z-0 h-full w-full' : 'z-0 h-4/5 w-full'}
           allowFullScreen />
       {:else}
         <img
@@ -360,3 +416,19 @@
     {/if}
   </div>
 </div>
+
+<dialog id="archiveAllConfirmation" class="rounded-lg bg-zinc-800 p-5 text-white">
+  <form>
+    <h1 class="text-2xl font-bold">Archive All</h1>
+    <p class="text-zinc-300">Are you sure you want to archive all new videos in your feed?</p>
+    <div class="mt-5 flex place-content-end gap-3">
+      <button
+        class="rounded-lg bg-red-900 px-4 py-2 font-bold text-red-300 transition duration-200 hover:bg-red-800"
+        on:click={archiveAll}>Yes, archive all</button>
+      <button
+        class="rounded-lg bg-zinc-700 px-4 py-2 font-bold text-zinc-300 transition duration-200 hover:bg-zinc-600"
+        formmethod="dialog"
+        value="cancel">Cancel</button>
+    </div>
+  </form>
+</dialog>
