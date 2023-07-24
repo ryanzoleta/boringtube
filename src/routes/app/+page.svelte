657f4104fa47329a3ab59cc93ec3e5b1103e00cd
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-07-24 20:08:54 +0800

    paddings

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -140,7 +140,7 @@
           {/each}
         </div>
 
-        <div class="relative flex flex-1 flex-col gap-3 overflow-scroll bg-zinc-950 px-3 pb-3">
+        <div class="relative flex flex-1 flex-col overflow-scroll bg-zinc-950 px-3 pb-3">
           <div class="sticky top-0 z-50 flex gap-2 bg-zinc-950 p-3">
             <button
               class="rounded-md px-4 py-1 transition duration-100 {view === 'feed'
@@ -165,75 +165,77 @@
                   class="spinner_P7sC" /></svg>
             </div>
           {:else if viewingVideoList}
-            {#each viewingVideoList as video}
-              <button
-                class="group relative flex flex-col rounded-lg bg-zinc-900 text-left text-white transition duration-200 hover:bg-zinc-800"
-                on:click={() => {
-                  playing = false;
-                  currentVideo = video;
-                }}>
-                <img src={video.thumbnail} alt="video thumbnail" class="w-full rounded-t-md" />
-
-                <div class="flex flex-col gap-1 p-3">
-                  <h3 class="line-clamp-2 font-bold leading-tight">{video.title}</h3>
-                  <div class="flex gap-1">
-                    <div class="w-5">
-                      <img
-                        src={video.channel.snippet.thumbnails.default.url}
-                        alt="channel avatar"
-                        class="rounded-full" />
-                    </div>
-                    <p class="my-auto text-xs text-zinc-500">
-                      {video.channel.snippet.title} • {moment(video.published).fromNow()}
-                    </p>
-                  </div>
-                </div>
+            <div class="flex flex-col gap-3">
+              {#each viewingVideoList as video}
                 <button
-                  class="absolute bottom-2 right-2 hidden w-8 rounded-md p-1 text-zinc-700 hover:bg-zinc-900 group-hover:block"
-                  on:click|stopPropagation={() => {
-                    if (view === 'feed') archiveVideo(video);
-                    else unarchiveVideo(video);
-                  }}>
-                  {#if view === 'feed'}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      class="h-full w-full">
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                    </svg>
-                  {:else}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      class="h-full w-full">
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
-                    </svg>
-                  {/if}
-                </button>
-              </button>
-            {/each}
-
-            {#if view === 'feed'}
-              <div>
-                <button
-                  class="rounded-full bg-zinc-800 px-4 py-2 text-white"
+                  class="group relative flex flex-col rounded-lg bg-zinc-900 text-left text-white transition duration-200 hover:bg-zinc-800"
                   on:click={() => {
-                    $videosQuery.fetchNextPage();
-                  }}>More...</button>
-              </div>
-            {/if}
+                    playing = false;
+                    currentVideo = video;
+                  }}>
+                  <img src={video.thumbnail} alt="video thumbnail" class="w-full rounded-t-md" />
+
+                  <div class="flex flex-col gap-1 p-3">
+                    <h3 class="line-clamp-2 font-bold leading-tight">{video.title}</h3>
+                    <div class="flex gap-1">
+                      <div class="w-5">
+                        <img
+                          src={video.channel.snippet.thumbnails.default.url}
+                          alt="channel avatar"
+                          class="rounded-full" />
+                      </div>
+                      <p class="my-auto text-xs text-zinc-500">
+                        {video.channel.snippet.title} • {moment(video.published).fromNow()}
+                      </p>
+                    </div>
+                  </div>
+                  <button
+                    class="absolute bottom-2 right-2 hidden w-8 rounded-md p-1 text-zinc-700 hover:bg-zinc-900 group-hover:block"
+                    on:click|stopPropagation={() => {
+                      if (view === 'feed') archiveVideo(video);
+                      else unarchiveVideo(video);
+                    }}>
+                    {#if view === 'feed'}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="h-full w-full">
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                      </svg>
+                    {:else}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="h-full w-full">
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                      </svg>
+                    {/if}
+                  </button>
+                </button>
+              {/each}
+
+              {#if view === 'feed'}
+                <div>
+                  <button
+                    class="rounded-full bg-zinc-800 px-4 py-2 text-white"
+                    on:click={() => {
+                      $videosQuery.fetchNextPage();
+                    }}>More...</button>
+                </div>
+              {/if}
+            </div>
           {/if}
         </div>
       {/if}
