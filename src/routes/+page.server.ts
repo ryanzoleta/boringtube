1b249104e9e7094038786f24ce2687eb63b0578e
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-07-25 21:29:24 +0800

    fix lint errors

--- src/routes/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { redirect, type RequestEvent } from '@sveltejs/kit';
-
-export async function load({ locals }: RequestEvent) {
-  const session = await locals.getSession();
-
-  if (session) {
-    // throw redirect(303, '/app');
-  }
-}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { page } from '$app/stores';
-  import { signIn, signOut } from '@auth/sveltekit/client';
+  import { signIn } from '@auth/sveltekit/client';
 </script>
 
 <main class="flex min-h-screen flex-col place-items-center bg-black">
